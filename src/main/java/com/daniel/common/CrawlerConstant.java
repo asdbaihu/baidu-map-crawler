1ad93735550edb5a431acaa9487a025e16116984
lingengxiang <lingengxiang@sensetime.com>
2018-12-13 16:40:52 +0800

    优化代码

--- src/main/java/com/daniel/common/CrawlerConstant.java
+++ src/main/java/com/daniel/common/CrawlerConstant.java
@@ -24,7 +24,7 @@ public class CrawlerConstant {
     /**
      * 百度地图API
      */
-    public static final String BAIDU_MAP_API = "http://api.map.baidu.com/place/v2/search?query=%s&tag=%s&region=%s&output=json&page_num=0&page_size=20&ak=%s";
+    public static final String BAIDU_MAP_API = "http://api.map.baidu.com/place/v2/search?query=%s&tag=%s&region=%s&output=json&coord_type=%s&page_num=0&page_size=20&ak=%s";
 
     /**
      * 百度地图API的访问token
@@ -40,5 +40,25 @@ public class CrawlerConstant {
      * 写入数据格式：一级分类,二级分类,名称,地址,经度,纬度,所属省份,所属城市,所属区县
      */
     public static final String DATA_OUTPUT_FORMAT = "%s,%s,%s,%s,%s,%s,%s,%s,%s";
+
+    /**
+     * 爬虫线程数
+     */
+    public static final Integer THREAD_NUM = 20;
+
+    /**
+     * 存放TAG数据的路径
+     */
+    public static final String TAG_PATH = "src/main/resources/tag.txt";
+
+    /**
+     * TAG数据一级分类的分词
+     */
+    public static final String TAG_TOP_SPLIT = "：";
+
+    /**
+     * TAG数据二级分类的分词
+     */
+    public static final String TAG_SUB_SPLIT = "、";
 
 }
